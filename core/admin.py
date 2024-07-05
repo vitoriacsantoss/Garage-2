7dd95cee9d99a6cf50d34e5bd75ea9f1d9d40318
vitoriacsantoss <santosviih360@gmail.com>
2024-07-05 14:43:27 -0300

    Finalização do Acessório e da Categoria

--- core/admin.py
+++ core/admin.py
@@ -50,3 +50,5 @@ class UserAdmin(BaseUserAdmin):
 
 
 admin.site.register(models.User, UserAdmin)
+admin.site.register(models.Acessorio)
+admin.site.register(models.Categoria)
---
+++ core/views/acessorio.py
@@ -0,0 +1,8 @@
+from rest_framework.viewsets import ModelViewSet
+
+from core.models import Acessorio
+from  core.serializers import AcessorioSerializer
+
+class AcessorioViewSet(ModelViewSet):
+    queryset = Acessorio.objects.order_by("descricao")
+    serializer_class = AcessorioSerializer
