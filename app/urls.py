a8489a495055061a481290116de4d155ea3271f2
vitoriacsantoss <santosviih360@gmail.com>
2024-07-05 15:24:35 -0300

    Finalizar API marcas

--- app/urls.py
+++ app/urls.py
@@ -9,7 +9,7 @@ from drf_spectacular.views import (
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from core.views import AcessorioViewSet, CategoriaViewSet, CorViewSet, UserViewSet
+from core.views import AcessorioViewSet, CategoriaViewSet, CorViewSet, MarcaViewSet, UserViewSet
 
 router = DefaultRouter()
 
@@ -17,6 +17,8 @@ router.register(r"usuarios", UserViewSet, basename="usuarios")
 router.register(r"acessorio", AcessorioViewSet, basename="acessorios")
 router.register(r"categorias", CategoriaViewSet, basename="categorias")
 router.register(r"cor", CorViewSet, basename="cores")
+router.register(r"marca", MarcaViewSet, basename="marcas")
+
 
 
 
